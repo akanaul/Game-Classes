bcedcdae760e7da194eafdd270ed382a9509d945
akanaul <luanalves0512@gmail.com>
2025-09-17 18:05:27 -0300

    Método ataque base

--- charClass.js
+++ charClass.js
@@ -4,4 +4,14 @@ class hero{
         this.idade = idade
         this.tipo = tipo
     }
+    atacar(){
+        let ataques = ["magia","espada","artes marciais","shuriken"]
+        if(this.tipo == "mago"){
+            return console.log(`O ${this.tipo} atacou usando ${ataques[0]}`)
+        }
+
 }
+}
+
+let heroIgnis = new hero("Ignis", 20, "mago")
+heroIgnis.atacar()
