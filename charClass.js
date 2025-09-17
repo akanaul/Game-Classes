fd757a2f62bdde139d7dc0e177773dba316f02c7
akanaul <luanalves0512@gmail.com>
2025-09-17 18:11:21 -0300

    Concluindo método atacar

--- charClass.js
+++ charClass.js
@@ -8,10 +8,16 @@ class hero{
         let ataques = ["magia","espada","artes marciais","shuriken"]
         if(this.tipo == "mago"){
             return console.log(`O ${this.tipo} atacou usando ${ataques[0]}`)
+        } else if (this.tipo == "guerreiro"){
+            return console.log(`O ${this.tipo} atacou usando ${ataques[1]}`)
+        } else if (this.tipo == "monge"){
+            return console.log(`O ${this.tipo} atacou usando ${ataques[2]}`)
+        } else if (this.tipo == "ninja"){
+            return console.log(`O ${this.tipo} atacou usando ${ataques[3]}`)
         }
 
 }
 }
-
-let heroIgnis = new hero("Ignis", 20, "mago")
+//tentar usando um método melhor outra hora para o ataque, talvez com uma estrutura de repetição
+let heroIgnis = new hero("Ignis", 20, "ninja")
 heroIgnis.atacar()
